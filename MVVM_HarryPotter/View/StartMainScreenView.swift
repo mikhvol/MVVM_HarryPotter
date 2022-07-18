7f36231051d89a1b960f9773a7da844562120c59
Mikhail Volkov <skylovecmt@gmail.com>
2022-07-18 22:18:31 +0300

    delete: delete strorydboard and customize new startVC

---
+++ MVVM_HarryPotter/View/StartMainScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  StartMainScreenView.swift
+//  MVVM_HarryPotter
+//
+//  Created by worker on 18.07.2022.
+//
+
+import UIKit
+
+class StartMainScreenView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
