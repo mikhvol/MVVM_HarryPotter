cf07c9eeec9cff330c386cadbed295cb7995f748
Mikhail Volkov <skylovecmt@gmail.com>
2022-07-18 20:50:24 +0300

    feat: Add project

---
+++ MVVM_HarryPotter/ServiceLayer/NetworkDataFetcher.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkDataFetcher.swift
+//  MVVM_HarryPotter
+//
+//  Created by worker on 18.07.2022.
+//
+
+import Foundation
+
+protocol DataFetcher {
+    func fetchGenericJSONData<T: Decodable>(urlString: String, response: @escaping (T?) -> Void)
+}
+
+class NetworkDataFetcher: DataFetcher {
+    
+    var networking: Networking
+    
+    init(networking: Networking = NetworkService()) {
+        self.networking = networking
+    }
+    
+    func fetchGenericJSONData<T: Decodable>(urlString: String, response: @escaping (T?) -> Void) {
+        self.networking.request(urlString: urlString) { (data, error) in
+            if let error = error {
+                print("Error recieved requesting data: \(error.localizedDescription)")
+                response(nil)
+            }
+            let decoded = self.decodeJSON(type: T.self, from: data)
+            response(decoded)
+        }
+    }
+    
+    func decodeJSON<T: Decodable>(type: T.Type, from: Data?) -> T? {
+        let decoder = JSONDecoder()
+        guard let data = from else { return nil }
+        do {
+            let objects = try decoder.decode(type.self, from: data)
+            return objects
+        } catch let jsonError {
+            print("Failed to decode JSON", jsonError)
+            return nil
+        }
+        
+    }
+    
+}
