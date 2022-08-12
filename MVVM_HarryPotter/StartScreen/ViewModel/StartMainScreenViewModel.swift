70391e70572f774c69ab2cf8b6b3176f7cc2b7a1
Mikhail Volkov <skylovecmt@gmail.com>
2022-08-12 14:13:40 +0300

    Minor refactoring

---
+++ MVVM_HarryPotter/StartScreen/ViewModel/StartMainScreenViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  StartMainScreenViewModel.swift
+//  MVVM_HarryPotter
+//
+//  Created by worker on 12.08.2022.
+//
+
+import Foundation
+
+struct StartMainScreenViewModel {
+    
+    //External dependencies
+    var dataFetcherService = DataFetcherService()
+    
+    func getCharachters(complition: @escaping ([CharacterHP]?)->()) {
+        self.dataFetcherService.fetchCharactersOfHarryPotter { (charachtersHP) in
+            guard let charachtersHP = charachtersHP else {
+                complition(nil)
+                return
+            }
+            
+            DispatchQueue.global().async {
+                complition(charachtersHP)
+            }
+        }
+    }
+}
--- MVVM_HarryPotter/StudentsScreen/ViewModel/StudentsScreenViewModel.swift
+++ MVVM_HarryPotter/StudentsScreen/ViewModel/StudentsScreenViewModel.swift
@@ -9,20 +9,4 @@ import Foundation
 import UIKit
 
 struct StudentsScreenViewModel {
-    
-    //External dependencies
-    var dataFetcherService = DataFetcherService()
-    
-    func getCharachters(complition: @escaping ([CharacterHP]?)->()) {
-        self.dataFetcherService.fetchCharactersOfHarryPotter { (charachtersHP) in
-            guard let charachtersHP = charachtersHP else {
-                complition(nil)
-                return
-            }
-            
-            DispatchQueue.global().async {
-                complition(charachtersHP)
-            }
-        }
-    }
 }
